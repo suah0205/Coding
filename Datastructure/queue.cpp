8e66ebf18c04ab35bc5899361d17e6e7c6b52248
VinaySingh035 <csvinay.d@gmail.com>
2016-02-08 15:09:03 +0530

    in'stlC++'

---
+++ Datastructure/queue.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <iostream>
+
+#include <queue>
+
+using namespace std;
+
+int main(){
+	queue <int> q;
+	int x,i,n;
+	char c;
+	while(1){
+	cin >> c;
+	if(c=='i' || c=='I'){
+		cin >> x;		
+		q.push(x);
+	}
+	else if(c=='d' || c=='D'){
+		x=q.front();
+		cout << x << endl;
+		q.pop();
+	}
+	else if(c=='p' || c=='P'){
+		n=q.size();
+		for(i=0;i<n;i++){
+			x=q.front();
+			cout << x << endl;
+			q.pop();
+			q.push(x);
+		}
+	}
+ 	else if(c=='q' || c=='Q'){
+ 		return 0;
+ 	}
+  }
+return 0;
+}
